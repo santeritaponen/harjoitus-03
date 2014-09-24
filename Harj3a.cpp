24a8ba041a68cba57f264e0197cab829df171230
santeritaponen <a@b.com>
2014-09-24 10:55:18 +0300

    versio1.0

---
+++ Harj3a.cpp
@@ -0,0 +1,17 @@
+/*#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nime‰ tiedosto harj3a.cpp nimelle!*/
+#include <iostream> // otsikkotiedosto sis‰llytet‰‰n ohjelmaan
+using namespace std;
+void main(void) // p‰‰ohjelma alkaa
+{
+	cout << "Santeri Taponen!\n";
+	cout << "Helvintie 2 b B 9";
+		}
